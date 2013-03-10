d40f2978acc5d639ccd93a1bc263daa9ec3bad4e
leo liang <leoliang_climber@hotmail.com>
2013-03-10 16:47:26 +1100

    use Bundler.bin_path to get the correct bin_path

--- etc/rbenv.d/which/rbenv-binstubs.bash
+++ etc/rbenv.d/which/rbenv-binstubs.bash
@@ -6,8 +6,9 @@ check_for_binstubs()
   root="$PWD"
   while [ -n "$root" ]; do
     if [ -e "$root/Gemfile" ]; then
-      if [ -x "$root/bin/$RBENV_COMMAND" ]; then
-	RBENV_COMMAND_PATH="$root/bin/$RBENV_COMMAND"
+      local bin_file=$(ruby -e 'require "rubygems";require "bundler"; puts Bundler.bin_path if Bundler.respond_to?(:bin_path);')
+      if [ -d "$bin_file" ] && [ -x "$bin_file/$RBENV_COMMAND" ]; then
+  RBENV_COMMAND_PATH="$bin_file/$RBENV_COMMAND"
       fi
       root=""
     fi
@@ -18,4 +19,3 @@ check_for_binstubs()
 if [ -z "$DISABLE_BINSTUBS" ]; then
   check_for_binstubs
 fi
-
